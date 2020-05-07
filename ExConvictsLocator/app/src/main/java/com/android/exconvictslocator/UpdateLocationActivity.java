7a50d32c095cb05250e483ee056cd5d36c6d6dc8
olgaSavic <lola.savi4@gmail.com>
2020-05-07 23:30:51 +0200

    Ekran za podesavanja

---
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/UpdateLocationActivity.java
@@ -0,0 +1,16 @@
+package com.android.exconvictslocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class UpdateLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_location);
+    }
+}
