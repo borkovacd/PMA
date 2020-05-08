0258ff2ae7addcf6adfe0398f5386cc82fbd8b17
Jovana Novakovic <jovana_novakovic@uns.ac.rs>
2020-05-08 23:49:13 +0200

    dodati tabovi za listu i mapu za sve osudjenike

---
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/PageAdapter.java
@@ -0,0 +1,36 @@
+package com.android.exconvictslocator;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numberOfTabs;
+    public PageAdapter(@NonNull FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.numberOfTabs = numberOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        System.out.println("CAO CAO "+ position);
+       switch (position){
+           case 0 : return  new tabMap();
+           case 1 : return  new tabList();
+           default: return null;
+       }
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}
---
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/tabList.java
@@ -0,0 +1,28 @@
+package com.android.exconvictslocator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class tabList extends Fragment {
+
+    public tabList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_list, container, false);
+    }
+}
