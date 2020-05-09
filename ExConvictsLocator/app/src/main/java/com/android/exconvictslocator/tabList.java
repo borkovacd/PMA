5634e73e493420fc3a1ca1bf40d69665cc010638
Jovana Novakovic <jovana_novakovic@uns.ac.rs>
2020-05-09 15:45:32 +0200

    lista osudjenika

--- ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/tabList.java
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/tabList.java
@@ -23,6 +23,8 @@ public class tabList extends Fragment {
     ListView listView;
     String convictsNames [] = {"Pera Perić", "Mika Mikić", "Žika Žikić"};
     String convictsCrimes [] = {"silovanje", "ubistvo", "ubistvo"};
+    String convictsLocations [] = {"Bulevar oslobodjenja, Beograd", "Železnička stanica, Novi Sad", "Zeleni venac, Beograd"};
+
     int convictsImages[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
 
 
@@ -44,7 +46,7 @@ public class tabList extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         listView = getView().findViewById(R.id.convicts_list_view);
-        MyAdapter adapter = new MyAdapter(this.getActivity(), convictsNames, convictsCrimes, convictsImages);
+        MyAdapter adapter = new MyAdapter(this.getActivity(), convictsNames, convictsCrimes, convictsImages,convictsLocations);
         listView.setAdapter(adapter);
     }
 
@@ -53,13 +55,15 @@ public class tabList extends Fragment {
         String rNames[];
         String rCrimes[];
         int rImgs[];
+        String rLocations[];
 
-        public MyAdapter(@NonNull Context context, String names[], String crimes[], int imgs[] ) {
+        public MyAdapter(@NonNull Context context, String names[], String crimes[], int imgs[], String locations[] ) {
             super(context, R.layout.row_in_list, R.id.convict_name, names);
             this.context = context;
             this.rCrimes = crimes;
             this.rNames = names;
             this.rImgs = imgs;
+            this.rLocations= locations;
 
         }
 
@@ -71,9 +75,11 @@ public class tabList extends Fragment {
             ImageView images = row.findViewById(R.id.convict_image);
             TextView names = row.findViewById(R.id.convict_name);
             TextView crimes = row.findViewById(R.id.convict_crime);
+            TextView locations = row.findViewById(R.id.convict_last_location);
             images.setImageResource(rImgs[position]);
             names.setText(rNames[position]);
             crimes.setText(rCrimes[position]);
+            locations.setText(rLocations[position]);
 
             return row;
         }
