02e0a9df12cb22f03553b851db598974988bb32c
olgaSavic <lola.savi4@gmail.com>
2020-05-10 14:45:37 +0200

    Izmene, povezivanje ekrana

--- ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/ExConvictDetailsActivity.java
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/ExConvictDetailsActivity.java
@@ -9,9 +9,21 @@ import android.widget.Button;
 
 public class ExConvictDetailsActivity extends AppCompatActivity {
 
+    Button btnPrijavi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex_convict_details);
+
+        btnPrijavi = findViewById(R.id.btn_prijavi);
+
+        btnPrijavi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ExConvictDetailsActivity.this, UpdateLocationActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
