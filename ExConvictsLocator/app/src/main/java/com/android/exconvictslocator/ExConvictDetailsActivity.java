af1441f1c283707875295314cad7e24818d359cf
Jovana Novakovic <jovana_novakovic@uns.ac.rs>
2020-05-11 21:25:38 +0200

    izmenjek ekran sa detaljima o osudjeniku

--- ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/ExConvictDetailsActivity.java
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/ExConvictDetailsActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ExConvictDetailsActivity extends AppCompatActivity {
 
     Button btnPrijavi;
+    Button btnAllLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,56 @@ public class ExConvictDetailsActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        btnAllLocations = findViewById(R.id.btn_sveLokacije);
+        TextView addressV = findViewById(R.id.convict_details_address);
+        TextView genderV = findViewById(R.id.convict_details_gender);
+        TextView birthV = findViewById(R.id.convict_details_birth);
+        TextView crimeV = findViewById(R.id.convict_details_crime);
+        TextView descriptionV = findViewById(R.id.convict_details_description);
+        TextView lastLocationV = findViewById(R.id.convict_details_last_location);
+        TextView updateTimeV = findViewById(R.id.convict_details_update_time);
+        TextView nameDetailV = findViewById(R.id.convict_details_name);
+        TextView nicknameDetailV = findViewById(R.id.convict_details_nickname);
+        ImageView imageDetailV = findViewById(R.id.convict_details_image);
+
+
+
+        Bundle b = getIntent().getExtras();
+        String name = null;
+        String nickname = null;
+        String address = null;
+        String birth = null;
+        String gender = null;
+        String crime = null;
+        String desc = null;
+        String lastLocation = null;
+        String updetedAt = null;
+        int img = -1;
+
+
+        if(b != null) {
+            name = b.getString("name");
+            nickname = b.getString("nickname");
+            img = b.getInt("image");
+             address = b.getString("address");
+             birth = b.getString("birth");
+            gender =b.getString("gender");
+             crime =b.getString("crime");
+             updetedAt = b.getString("updetedAt");
+             desc = b.getString("desc");
+             lastLocation =b.getString("lastLocation");
+        }
+     addressV.setText(address);
+    genderV.setText(gender);
+         birthV.setText(birth);
+      crimeV.setText(crime);
+       descriptionV.setText(desc);
+        lastLocationV.setText(lastLocation);
+        updateTimeV.setText(updetedAt);
+       nameDetailV.setText(name);
+        nicknameDetailV.setText(nickname);
+       imageDetailV.setImageResource(img);
+
     }
 }
