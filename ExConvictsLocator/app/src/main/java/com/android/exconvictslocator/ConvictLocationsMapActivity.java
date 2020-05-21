c9fe959742276295851333dcdbb1271a0476b5c0
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2020-05-21 12:05:39 +0200

    Završena podešavanja Navigation Drawer-a

--- ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/ConvictLocationsMapActivity.java
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/ConvictLocationsMapActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,15 +24,17 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ConvictLocationsMapActivity extends FragmentActivity  {
+public class ConvictLocationsMapActivity extends MainActivity  {
+
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_convict_locations_map);
-        ConvictLocationsMapsFragment mapsFragment = new ConvictLocationsMapsFragment();
-        Bundle b = getIntent().getExtras();
-        String name = null; // or other values
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View contentView = inflater.inflate(R.layout.activity_convict_locations_map, null, false);ConvictLocationsMapsFragment mapsFragment = new ConvictLocationsMapsFragment();
+                mDrawer = (DrawerLayout) findViewById(R.id.drawer);Bundle b = getIntent().getExtras();
+                mDrawer.addView(contentView, 0);String name = null; // or other values
         String nickname = null; // or other values
         int img = -1; // or other values
 
