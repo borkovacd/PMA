0163285be45d94254c0ac41cf2996d6604035612
olgaSavic <lola.savi4@gmail.com>
2020-06-06 18:37:22 +0200

    Zapoceta sinhronizacija

---
+++ pma-sync/src/test/java/com/ftn/pmasync/PmaSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.pmasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmaSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
