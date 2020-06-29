59521aa4fb6153a45cce17a252865531e27b491d
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2020-06-29 02:51:39 +0200

    Dodato i preuzimanje usera i prijava lokacija, izmena Spring projekta, podešavanje automatske detekcije WiFi IP adrese

--- pma-sync/src/main/java/com/ftn/pmasync/controller/ReportController.java
+++ pma-sync/src/main/java/com/ftn/pmasync/controller/ReportController.java
@@ -1,7 +1,9 @@
 package com.ftn.pmasync.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ftn.pmasync.dto.ExConvictDTO;
+import com.ftn.pmasync.dto.ReportDTO;
+import com.ftn.pmasync.model.ExConvict;
 import com.ftn.pmasync.model.Report;
 import com.ftn.pmasync.repository.ReportRepository;
+import com.ftn.pmasync.service.ReportService;
 
 @RestController
 @RequestMapping(path = "/api/reports")
 public class ReportController {
 	
+	@Autowired
+	private ReportService reportService ;
+	//Brisi ovaj repository cole mu :D
 	@Autowired
 	private ReportRepository reportRepository ;
 	
@@ -33,5 +42,19 @@ public class ReportController {
 		else // ako nije uspeo da ga sacuva
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ReportDTO>> getReports() {
+		List<ReportDTO> reportsDTO = new ArrayList<ReportDTO>();
+		List<Report> reports = reportService.getReports();
+		for(Report report : reports) {
+			ModelMapper modelMapper = new ModelMapper();
+			ReportDTO reportDTO = modelMapper.map(report, ReportDTO.class);
+			reportsDTO.add(reportDTO);
+		}
+		return new ResponseEntity<List<ReportDTO>> (reportsDTO, HttpStatus.OK);		
+	}
+	
+	
 
 }
