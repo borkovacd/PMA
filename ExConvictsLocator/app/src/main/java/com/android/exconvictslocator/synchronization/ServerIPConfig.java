8d939ef68ccf4e2f436f8799c92c0618bccf3134
olgaSavic <lola.savi4@gmail.com>
2020-07-03 02:51:58 +0200

    Landscape view

--- ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/synchronization/ServerIPConfig.java
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/synchronization/ServerIPConfig.java
@@ -3,7 +3,7 @@ package com.android.exconvictslocator.synchronization;
 public class ServerIPConfig {
 
     private static String ip_address = "192.168.0.16";
-
+//     private static String ip_address = "192.168.0.16";  - Olga
     public static String getIp_address() {
         return ip_address;
     }
