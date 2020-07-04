3e104d6dd9f20303277b715cd4e53eb87bca8599
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2020-07-04 23:11:46 +0200

    Finalna APK verzija

--- ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/synchronization/ServerIPConfig.java
+++ ExConvictsLocator/app/src/main/java/com/android/exconvictslocator/synchronization/ServerIPConfig.java
@@ -2,9 +2,8 @@ package com.android.exconvictslocator.synchronization;
 
 public class ServerIPConfig {
 
-    private static String ip_address = "192.168.0.80";
-    //private static String ip_address = "192.168.43.226"; //MOBILE HOTSPOT
-    //private static String ip_address = "192.168.0.16";  // - Olga
+    private static String ip_address = "192.168.43.226"; //MOBILE HOTSPOT
+
     public static String getIp_address() {
         return ip_address;
     }
